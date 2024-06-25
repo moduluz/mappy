2209c982a513c28a9d74efc4d75b3b0a8e1a7cd8
Aditya Singh Khichi <97791070+moduluz@users.noreply.github.com>
2024-06-25 18:06:07 +0530

    Add files via upload

---
+++ india.js
@@ -0,0 +1,669 @@
+const choice_location = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Gudivada, Andhra Pradesh, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [80.992630, 16.441025]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Dhinoj, Patan, Gujarat, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.279541, 23.661324]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Idappadi, Salem, Tamil Nadu, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [77.849998, 11.580000]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Chitalwana, Rajasthan, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [71.659721, 24.924999]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Pugal, Rajasthan, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.800003, 28.500000]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Doom Dooma, Assam, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [95.570000, 27.570000]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Usar, Maharashtra, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [73.131744, 18.291109]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Sirsi, Karnataka, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [74.835403, 14.619500]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Perinad, Kerala, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [76.640022, 8.956280]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "New Tehri, Uttarakhand, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [78.480003, 30.379999]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Cheekode, Kerala, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [75.989342, 11.230430]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Palani, Dindigul, Tamil Nadu, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [77.519997, 10.450000]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Theni Allinagaram, Theni, Tamil Nadu, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [77.478615, 10.011944]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Bhawani Mandi, Rajasthan, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [75.829720, 24.420000]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Beldanga, West Bengal, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [88.250000, 23.930000]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Vedasandur, Tamil Nadu, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [77.922646, 10.530206]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Paniyara, Uttar Pradesh, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [83.463402, 26.997454]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Nagarbera, Assam, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [91.003578, 26.111790]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Atgaon, Maharashtra, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [73.330368, 19.502623]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Malur, Karnataka, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [77.937111, 12.990000]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Munnar, Idukki, Kerala, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [77.059723, 10.089167]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Demjok, Jammu and Kashmir, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.449516, 32.699909]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Umerkote, Odisha, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [82.211998, 19.665001]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Mohone, Kalyan, Thane, Maharashtra, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [73.172371, 19.265133]
+        }
+      },
+
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Kuwerpur, Uttar Pradesh, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [78.118172, 27.217766]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Holnanthe, Pashte, Maharashtra, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [74.900551, 21.204906]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Asopur, Rukanpura, Patna, Bihar, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [85.043251, 25.58942]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Sagardighi, West Bengal, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [88.086357, 24.292355]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Siolim, Goa, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [73.777222, 15.62009]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Rahika, Bihar, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [86.020332, 26.378592]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Veerabhadrapeta, Andhra Pradesh, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [83.194374, 18.038063]
+        }
+      },
+      {
+        "type": "Feature",
+      "properties": {
+        "name": "Gudivada, Andhra Pradesh, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [80.99263, 16.441025]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Dhinoj, Patan, Gujarat, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [72.279541, 23.661324]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Idappadi, Salem, Tamil Nadu, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [77.849998, 11.58]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Chitalwana, Rajasthan, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [71.659721, 24.924999]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Pugal, Rajasthan, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [72.800003, 28.5]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Doom Dooma, Assam, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [95.57, 27.57]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Usar, Maharashtra, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [73.131744, 18.291109]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Sirsi, Karnataka, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [74.835403, 14.6195]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Perinad, Kerala, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [76.640022, 8.95628]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "New Tehri, Uttarakhand, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [78.480003, 30.379999]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Cheekode, Kerala, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [75.989342, 11.23043]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Palani, Dindigul, Tamil Nadu, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [77.519997, 10.45]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Theni Allinagaram, Theni, Tamil Nadu, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [77.478615, 10.011944]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Bhawani Mandi, Rajasthan, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [75.82972, 24.42]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Beldanga, West Bengal, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [88.25, 23.93]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Vedasandur, Tamil Nadu, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [77.922646, 10.530206]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Paniyara, Uttar Pradesh, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [83.463402, 26.997454]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Nagarbera, Assam, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [91.003578, 26.11179]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Atgaon, Maharashtra, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [73.330368, 19.502623]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Malur, Karnataka, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [77.937111, 12.99]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Munnar, Idukki, Kerala, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [77.059723, 10.089167]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Demjok, Jammu and Kashmir, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [79.449516, 32.699909]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Umerkote, Odisha, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [82.211998, 19.665001]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Mohone, Kalyan, Thane, Maharashtra, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [73.172371, 19.265133]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Shiroor, Karnataka, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [74.599342, 13.916197]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Kuwerpur, Uttar Pradesh, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [78.118172, 27.217766]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Holnanthe, Pashte, Maharashtra, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [74.900551, 21.204906]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Asopur, Rukanpura, Patna, Bihar, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [85.043251, 25.58942]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Sagardighi, West Bengal, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [88.086357, 24.292355]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Siolim, Goa, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [73.777222, 15.62009]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Rahika, Bihar, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [86.020332, 26.378592]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Veerabhadrapeta, Andhra Pradesh, India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [83.194374, 18.038063]
+      }
+    },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Shiroor, Karnataka, India"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [74.599342, 13.916197]
+        }
+      }
+    ]
+  };
+  
+
+// Function to remove duplicates based on 'name' property
+function removeDuplicates(features) {
+  const uniqueFeatures = [];
+  const uniqueNames = new Set();
+
+  features.forEach(feature => {
+    const name = feature.properties.name;
+    if (!uniqueNames.has(name)) {
+      uniqueNames.add(name);
+      uniqueFeatures.push(feature);
+    }
+  });
+
+  return uniqueFeatures;
+}
+
+// Remove duplicates
+choice_location.features = removeDuplicates(choice_location.features);
+
+// Log the updated GeoJSON data
+console.log(JSON.stringify(choice_location, null, 2));
